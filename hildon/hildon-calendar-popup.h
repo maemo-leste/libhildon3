1203e52fac1b84d03b133039c2091d57bc3d8909
Robert Irvin <android_808@hotmail.com>
2017-01-26 17:57:04 +0000

    [FIX][GTK3] hildon-calendar-popup: We can only include gtk.h now.

--- hildon/hildon-calendar-popup.h
+++ hildon/hildon-calendar-popup.h
@@ -27,7 +27,7 @@
 #ifndef                                         __HILDON_CALENDAR_POPUP_H__
 #define                                         __HILDON_CALENDAR_POPUP_H__
 
-#include                                        <gtk/gtkdialog.h>
+#include                                        <gtk/gtk.h>
 
 G_BEGIN_DECLS
 
